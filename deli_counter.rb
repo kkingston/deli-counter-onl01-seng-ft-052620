5be84a0cf84a2fde375720f5756eabc17784c173
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 23:26:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,9 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
-  else katz_deli.shift 
+  katz_deli.shift 
     puts "Currently serving #{katz_deli}."
+    puts "There is nobody waiting to be served!"
   
 end
   
