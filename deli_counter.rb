1fb9cc2b63db5922408a01780435d3549b47f03a
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 23:33:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length >= 1
-    katz_deli.shift
+    katz_deli
     puts "Currently serving #{katz_deli.first}."
   else katz_deli.length == 0
     puts "There is nobody waiting to be served!"
