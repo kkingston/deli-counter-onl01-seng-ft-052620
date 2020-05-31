e8118a1af57a1b69f59575d462e0904d8c4aefe5
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 22:55:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(katz_deli)
   end
   
   if katz_deli.length > 0 
-    puts "The line is currently: #{katz_deli_updated}"puts "The line is currently empty."
+    puts "The line is currently: #{katz_deli_updated}"
+    puts "The line is currently empty."
     
   end
 end 
