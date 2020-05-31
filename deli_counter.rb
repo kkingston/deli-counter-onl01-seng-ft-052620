c439c60ee73c9fd84363047256b34710ee3cec89
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 22:50:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   end
 end 
 
-def take_a_number(katz_deli, name)  
-  katz_deli << 
-  puts "Welcome, #{person} you are number #{number} in line."
+# def take_a_number(katz_deli, name)  
+#   katz_deli << 
+#   puts "Welcome, #{person} you are number #{number} in line."
 end 
