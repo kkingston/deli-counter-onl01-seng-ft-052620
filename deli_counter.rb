81295e0e1a86090da8499d1102b94eb56939d4c4
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 22:49:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
 def line(katz_deli) 
-  katz_deli_updated = katz_deli.map.with_index(1) do |name, index|
+  katz_deli_updated = katz_deli.map.with_index(1) do |name, index| "#{index}. #{name}"
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  
+  else puts ""
   end
 end 
 
